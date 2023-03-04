1d727a1d15220145dbd293982c6794c825bde0b8
huhuiyu <1069306849@qq.com>
2023-03-04 09:39:19 +0800

    项目初始化

---
+++ push.ps1
@@ -0,0 +1,7 @@
+
+# github =====> git remote add github git@github.com:huhuiyu/huhuiyu.template.project.git
+# jihulab =====> git remote add jihulab git@jihulab.com:DarkKnight/huhuiyu.template.project.git
+# gitee =====> git remote add gitee git@gitee.com:huhuiyu/huhuiyu.template.project.git
+git push
+git push jihulab
+git push gitee
