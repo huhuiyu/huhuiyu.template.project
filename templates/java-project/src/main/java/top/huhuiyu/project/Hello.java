47193726161ffcb7999173be61e98af1658cfff1
huhuiyu <1069306849@qq.com>
2023-05-30 15:24:55 +0800

    合并gradle和maven项目模板

---
+++ templates/java-project/src/main/java/top/huhuiyu/project/Hello.java
@@ -0,0 +1,7 @@
+package top.huhuiyu.project;
+
+public class Hello {
+  public static void main(String[] args) {
+    System.out.println("Java你好！");
+  }
+}
---
+++ templates/java-project/src/test/java/top/huhuiyu/project/MainTest.java
@@ -0,0 +1,10 @@
+package top.huhuiyu.project;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+  @Test
+  public void test() {
+    System.out.println("测试运行");
+  }
+}
