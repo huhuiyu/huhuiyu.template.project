d167b1f6d17d033d9994771cb0d571df7cdb733c
huhuiyu <1069306849@qq.com>
2023-06-14 17:32:35 +0800

    maven-springboot2的员工dao查询完成

---
+++ templates/maven-springboot2/src/main/java/top/huhuiyu/template/maven/springboot2/dao/TbEmployeeMapper.java
@@ -0,0 +1,13 @@
+package top.huhuiyu.template.maven.springboot2.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import top.huhuiyu.template.maven.springboot2.entity.TbEmployee;
+
+import java.util.List;
+
+@Mapper
+public interface TbEmployeeMapper {
+  List<TbEmployee> queryAll(TbEmployee employee) throws Exception;
+
+  List<TbEmployee> queryAllAssociation(TbEmployee employee) throws Exception;
+}
